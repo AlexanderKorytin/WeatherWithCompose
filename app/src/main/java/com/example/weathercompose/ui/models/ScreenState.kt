68c42723ee2e267ae9decbc5cffe196c48d6aad2
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-05 15:46:36 +0300

    - рефакторинг

--- app/src/main/java/com/example/weathercompose/ui/models/ScreenState.kt
+++ app/src/main/java/com/example/weathercompose/ui/models/ScreenState.kt
@@ -2,7 +2,7 @@ package com.example.weathercompose.ui.models
 
 import com.example.weathercompose.domain.models.Current
 
-sealed interface ScreenState{
-    data class ErrorState(val message: String): ScreenState
-    data class ContentState(val result: Current): ScreenState
+sealed interface ScreenState {
+    data class ErrorState(val message: String) : ScreenState
+    data class ContentState(val result: Current) : ScreenState
 }
