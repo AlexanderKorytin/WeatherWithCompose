0234ad8aaa36c0bce831e1c60a730ee2fe370570
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-07 21:18:20 +0300

    - рефакторинг

--- app/src/main/java/com/example/weathercompose/ui/screens/main/mainStart.kt
+++ app/src/main/java/com/example/weathercompose/ui/screens/main/mainStart.kt
@@ -1,6 +1,7 @@
 package com.example.weathercompose.ui.screens.main
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,10 +14,15 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 fun mainCardStart() {
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
         val (bar) = createRefs()
-        CircularProgressIndicator(modifier = Modifier.constrainAs(bar) {
-            top.linkTo(parent.top, margin = 32.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        })
+        CircularProgressIndicator(
+            modifier = Modifier
+                .size(100.dp)
+                .constrainAs(bar) {
+                    top.linkTo(parent.top, margin = 32.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            strokeWidth = 10.dp
+        )
     }
 }
